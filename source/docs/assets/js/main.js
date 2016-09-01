2e38050208df7dc45965a2e01240284fa4675dd0
Rachel <rachel@getpantheon.com>
2016-09-01 12:05:19 -0500

    Update project dependencies and require zeroclipboard

--- source/docs/assets/js/main.js
+++ source/docs/assets/js/main.js
@@ -3,3 +3,9 @@
       //Calls the tocify method on your HTML div.
       $("#toc").tocify();
   });
+
+  var client = new ZeroClipboard( $("span#scenario1button_create-backup") );
+  var client = new ZeroClipboard( $("span#scenario1button_deploydev") );
+  var client = new ZeroClipboard( $("span#scenario1button_deploytest") );
+  var client = new ZeroClipboard( $("span#scenario2button_clear-cache") );
+  var client = new ZeroClipboard( $("span#scenario3button_scenario3") );
